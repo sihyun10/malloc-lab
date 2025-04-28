fbd9bbec40592e33bcc64d948d383e4c0b6a60ac
sihyun10 <hyhy200170@gmail.com>
2025-04-29 01:31:11 +0900

    feat: coalesce() 함수 구현 #3
    이 함수는 free된 블록을 기준으로 앞, 뒤의 블록의 할당 여부를 확인 후,
    4가지 경우의 수에서 해당되는 경우의 수를 병합한 뒤 더 큰 free 블록을 만들어주는 함수이다.

--- mm.c
+++ mm.c
@@ -143,3 +143,34 @@ static void *extend_heap(size_t words) {
 
   return coalesce(bp);
 }
+
+static void *coalesce(void *bp) {
+  size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+  size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+  size_t size = GET_SIZE(HDRP(bp));
+
+  if (prev_alloc && next_alloc) {
+    return bp;
+  }
+
+  else if (prev_alloc && !next_alloc) {
+    size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+  }
+
+  else if (!prev_alloc && next_alloc) {
+    size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+    bp = PREV_BLKP(bp);
+  }
+
+  else {
+    size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+    PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+    bp = PREV_BLKP(bp);
+  }
+  return bp;
+}
