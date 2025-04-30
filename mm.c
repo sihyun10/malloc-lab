6b155287cd0ff224b06b562603ad9fa39a160b5c
sihyun10 <hyhy200170@gmail.com>
2025-04-30 13:32:53 +0900

    refactor: 할당할 곳을 찾지 못했을때, 처음부터 다시한번 탐색 #4
    할당할 곳을 찾지 못하면 힙을 늘려 할당해주는데,
    만약에 이전 앞에 부분에 할당할 공간이 있을 수도 있다. 마냥 힙을 늘려 할당하는게 아닌,
    앞에 할당할 곳이 있으면 해주면 된다. 하지만 앞부분에도 할당할 곳이 없다면 이 로직은
    탐색 시간이 좀 걸릴 수 있지만, 넓게 보면 유용할 수 있다.

--- mm.c
+++ mm.c
@@ -222,6 +222,14 @@ static void *find_fit(size_t asize) {
     }
   }
 
+  /* 힙의 처음부터 next_fit_ptr까지 탐색 */
+  for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0 && bp != next_fit_ptr; bp = NEXT_BLKP(bp)) {
+    if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+      next_fit_ptr = bp;
+      return bp;
+    }
+  }
+
   return NULL;
 }
 
